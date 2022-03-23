ed38828ad0efbd232434db20f7f7520e7cb7bcc6
coderli17710241411 <102162804+coderli17710241411@users.noreply.github.com>
2022-03-23 11:29:03 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -6,8 +6,13 @@ alert("Hello JavaScript");
 
 window.onload=function(){
 let targetImage = document.querySelector("#smart-image");
+
 targetImage.addEventListener("click", function() {
-    alert("点击了图片");
+if (targetImage.classList.contains("small")) {
+targetImage.classList.remove("small");
+} else {
+targetImage.classList.add("small");
+}
 });
 }
 
