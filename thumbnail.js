25a82f5474d2ca61e5ef810652354e8c75b5ffa2
coderli17710241411 <102162804+coderli17710241411@users.noreply.github.com>
2022-03-23 11:01:17 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(event) {
 let targetImage = document.querySelector("#smart-image");
 
 targetImage.addEventListener("click", function() {
@@ -7,3 +8,5 @@ targetImage.classList.remove("small");
 targetImage.classList.add("small");
 }
 });
+});
+
